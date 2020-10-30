2bf16a2c05823e9e8290a448d80159b948566f52
zzc <916729656@qq.com>
2020-10-30 13:53:37 +0800

    最长公共前缀

--- 力扣项目/leetcode.h
+++ 力扣项目/leetcode.h
@@ -8,12 +8,20 @@ using namespace std;
 class leetcode
 {
 public:
-	//两数之和
+	//1 两数之和
 	vector<int> leetcode_twoSum1(vector<int>& nums, int target);
-	//整数反转
+	//7 整数反转
 	int leet_reverse7(int x);
+	//9 整数是否为回文数
+	bool leet_isPalindrome9(int x);
+	//13 罗马数字转换
+	int leet_romanToInt13(string s);
+	//14 最长公共前缀
+	string leet_longestCommonPrefix14(vector<string>& strs);
 protected:
 private:
+	//13 罗马数字转换
+	int change13(char var);
 };
 
 std::vector<int> leetcode::leetcode_twoSum1(vector<int>& nums, int target)
@@ -41,4 +49,69 @@ int leetcode::leet_reverse7(int x)
 		x /= 10;
 	}
 	return num > INT_MAX || num < INT_MIN ? 0 : num;
+}
+
+bool leetcode::leet_isPalindrome9(int x)
+{
+	//当x小于0，或者末尾为0时肯定不为0，x=0除外
+	if (x < 0 || (x % 10 == 0 && x != 0))
+		return false;
+	int num = 0;
+	//只进行反转一半
+	while (num < x)
+	{
+		num = num * 10 + x % 10;
+		x /= 10;
+	}
+	//判断是偶数还是奇数，奇数：12321 — 12 123 
+	return x == num || x == (num / 10);
+}
+
+int leetcode::leet_romanToInt13(string s)
+{
+	int sum = 0;
+	for (int i = 0; i < s.size(); i++)
+	{
+		int num = change13(s[i]);
+		if (s[i + 1] != '\0')
+			num = num > change13(s[i+1]) ? num : -num;
+		sum += num;
+	}
+	return sum;
+}
+
+std::string leetcode::leet_longestCommonPrefix14(vector<string>& strs)
+{
+	string result = "";
+	char var;
+	if (strs.size() == 0)
+		return result;
+	for (int i = 0; i < strs[0].size(); i++)
+	{
+		var = strs[0][i];
+		for (int j = 1; j < strs.size(); j++)
+		{
+			if (i < strs[j].size() && var == strs[j][i])
+				continue;
+			else
+				return result;
+		}
+		result += strs[0][i];
+	}
+	return result;
+}
+
+int leetcode::change13(char var)
+{
+	switch (var)
+	{
+	case 'I': return 1; break;
+	case 'V': return 5; break;
+	case 'X': return 10; break;
+	case 'L': return 50; break;
+	case 'C': return 100; break;
+	case 'D': return 500; break;
+	case 'M': return 1000; break;
+	}
+	return 0;
 }
