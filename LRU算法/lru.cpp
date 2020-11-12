f6c1b17da36192ca5ca0bde805f6744a1892a6c2
zzc <916729656@qq.com>
2020-11-12 11:05:34 +0800

    lru算法

--- LRU算法/lru.cpp
+++ LRU算法/lru.cpp
@@ -4,6 +4,10 @@
 #include<unordered_map>
 using namespace std;
 
+
+/*
+lru算法原理，最新的元素放在最开始，访问后的元素也放在最开始
+*/
 template<class KeyType, class ValueType>
 class LRUCache
 {
@@ -27,14 +31,21 @@ public:
 
 	void put(KeyType key, ValueType value)
 	{
-		if (m_map.count(key) >= 1)
+		if (m_map.count(key) >= 1) //访问匹配给当前键值的元素个数
 		{
 			Iter iter = m_map[key];
 			iter->second = value;
-			m_list.splice(m_list.begin(), m_list, iter);
+			m_list.splice(m_list.begin(), m_list, iter);// 将当前元素放置到链表的最开始位置
 		}
 		else
 		{
+			//如果当前插入新的键值对应的数据对
+			/*
+			1.判断当前链表是否超过最大容量
+				超过则删除最后一个元素
+				map中也删除对应键值
+			2 在链表前插入新加入的键值对
+			*/
 			Node node(key, value);
 			if (m_list.size() == m_capacity)
 			{
@@ -73,7 +84,7 @@ void show_func(int* a)
 int main()
 {
 	int value;
-	LRUCache<std::string, int> cache(5);
+	LRUCache<std::string, int> cache(5);// 构造 容量为5
 	cache.put("aaa", 1);
 	cache.put("bbb", 2);
 	cache.put("ccc", 3);
@@ -81,14 +92,18 @@ int main()
 	cache.put("eee", 5);
 
 	cache.show(show_func);
+	cout << endl;
 
 	cache.get("ccc", value);
+	cout  << endl;
 
 	cache.show(show_func);
+	cout << endl;
 
 	cache.put("fff", 6);
 
 	cache.show(show_func);
+	cout << endl;
 
 	system("pause");
 	return 0;
