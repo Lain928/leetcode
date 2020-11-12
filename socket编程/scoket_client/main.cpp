3547765f7f4ea4df82a3666123a891d29b84dfe6
zzc <916729656@qq.com>
2020-11-12 17:51:59 +0800

    修改socket

--- socket编程/scoket_client/main.cpp
+++ socket编程/scoket_client/main.cpp
@@ -26,9 +26,14 @@ int main()
 		printf("客户端的嵌套字打开失败!\n");
 		return 0;//结束  
 	}
+
+	//第一步 建立套接字
+
 	//创建socket这个关键词，这里想一下那个图形中的socket抽象层  
 	//注意socket这个函数，他三个参数定义了socket的所处的系统，socket的类型，以及一些其他信息  
 	SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	// 第二步 设置地址 要连接的地址
 
 	//socket编程中，它定义了一个结构体SOCKADDR_IN来存计算机的一些信息，像socket的系统，  
 	//端口号，ip地址等信息，这里存储的是服务器端的计算机的信息  
@@ -39,20 +44,26 @@ int main()
 
 	while (1)
 	{
+		//第三步 建立连接
 
 		//前期定义了套接字，定义了服务器端的计算机的一些信息存储在clientsock_in中，  
 		//准备工作完成后，然后开始将这个套接字链接到远程的计算机  
 		//也就是第一次握手  
 		connect(clientSocket, (SOCKADDR*)&clientsock_in, sizeof(SOCKADDR));//开始连接  
 
+		// 第四步 接收传输的数据信息
+
 		char receiveBuf[101];
 		//接收socket里面的内容  
 		recv(clientSocket, receiveBuf, 101, 0);
+		cout << "接收到的数据为：" << endl;
 		printf("%s\n", receiveBuf);
 
 		//发送socket数据  
 // 		cout << "向服务器端发送信息" << endl;
 // 		send(clientSocket, "hello,this is client", strlen("hello,this is client") + 1, 0);
+
+		// 第五步 关闭套接字 完成一次传输
 
 		//关闭套接字  
 		closesocket(clientSocket);
--- socket编程/scoket_server/main.cpp
+++ socket编程/scoket_server/main.cpp
@@ -10,6 +10,7 @@
 #pragma comment(lib,"ws2_32.lib")  
 using namespace std;
 
+//将打开一个exe文件
 void add_exe()
 {
 	/*
@@ -36,6 +37,8 @@ void add_exe()
 
 int main()
 {
+	// 第0步 建立套接字之前的一些操作
+
 	//创建套接字，socket前的一些检查工作，包括服务的启动  
 	WORD myVersionRequest;
 	WSADATA wsaData;
@@ -52,12 +55,21 @@ int main()
 		printf("嵌套字未打开!");
 		return 0;
 	}
+
+	//第一步 创建套接字
+
 	SOCKET serSocket = socket(AF_INET, SOCK_STREAM, 0);//创建了可识别套接字  
+
+	// 第二步 设置要连接的地址
+
 	//需要绑定的参数，主要是本地的socket的一些信息。  
 	SOCKADDR_IN addr;
 	addr.sin_family = AF_INET;
 	addr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);//ip地址  
-	addr.sin_port = htons(6000);//绑定端口  
+	addr.sin_port = htons(6000);//绑定端口
+
+
+	// 第四步 bind将地址赋值给套接字 并建立 监听（listen）
 
 	bind(serSocket, (SOCKADDR*)&addr, sizeof(SOCKADDR));//绑定完成  
 	listen(serSocket, 5);//其中第二个参数代表能够接收的最多的连接数  
@@ -69,14 +81,17 @@ int main()
 	{
 		cout << "等待客户端开启服务" << endl;
 		SOCKET serConn = accept(serSocket, (SOCKADDR*)&clientsocket, &len);//如果这里不是accept而是conection的话。。就会不断的监听 
-		char sendBuf[100] = "this is server";
+		char sendBuf[100] = "err";
+		cout << "请输入要传输到客户端的数据：" << endl;
+		cin >> sendBuf;
 		//sprintf(sendBuf, "welcome %s to bejing", inet_ntoa(clientsocket.sin_addr));//找对对应的IP并且将这行字打印到那里  
 		//发送信息  
 		send(serConn, sendBuf, strlen(sendBuf) + 1, 0);
-		cout << "接收客户端发送过来的消息：" << endl;
-		char receiveBuf[100];
-		recv(serConn, receiveBuf, strlen(receiveBuf) + 1, 0);
-		printf("%s\n", receiveBuf);
+
+// 		cout << "接收客户端发送过来的消息：" << endl;
+// 		char receiveBuf[100];
+// 		recv(serConn, receiveBuf, strlen(receiveBuf) + 1, 0);
+// 		printf("%s\n", receiveBuf);
 		closesocket(serConn);//关闭  
 		WSACleanup();//释放资源的操作  
 
